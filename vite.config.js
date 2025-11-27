55819e125ec5f386479d4251c35185f72319b61f
alvaroezequielpereyra02-sketch <alvaroezequielpereyra02@gmail.com>
2025-11-27 00:43:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
-{
-  "framework": "vite",
-  "buildCommand": "npm run build",
-  "outputDirectory": "dist"
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1600,
+  }
+})
+
+
