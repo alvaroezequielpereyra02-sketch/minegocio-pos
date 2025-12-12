57a38dd16a65a4aa49008b70f40d55b0a602ccdb
alvaro301 <alvaroezequielpereyra02@gmail.com>
2025-12-12 17:29:04 -0300

    Fixxed visual

--- src/App.jsx
+++ src/App.jsx
@@ -345,7 +345,7 @@ export default function App() {
           <button onClick={() => toggleModal('logout', true)} className="bg-slate-100 p-2 rounded-full"><LogOut size={18} /></button>
         </header>
 
-        <main className="flex-1 overflow-hidden p-4 relative z-0 flex flex-col">
+        <main className="flex-1 overflow-hidden p-4 pb-24 lg:pb-4 relative z-0 flex flex-col">
           {activeTab === 'pos' && (
             <div className="flex flex-col h-full lg:flex-row gap-4 overflow-hidden relative">
               <ProductGrid products={products} addToCart={addToCart} searchTerm={searchTerm} setSearchTerm={setSearchTerm} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} categories={categories} subcategories={subcategories} userData={userData} barcodeInput={barcodeInput} setBarcodeInput={setBarcodeInput} handleBarcodeSubmit={(e) => { e.preventDefault(); if (!barcodeInput) return; const p = products.find(x => x.barcode === barcodeInput); if (p) { addToCart(p); setBarcodeInput(''); } else alert("No encontrado"); }} />
